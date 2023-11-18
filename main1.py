dfc4e15a11484db716beae5953d0f5d4da18394f
asdsn2121 <149259341+asdsn2121@users.noreply.github.com>
2023-11-18 17:44:52 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,5 +1,4 @@
 import discord
-import random
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -7,25 +6,25 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
+async def bye(ctx):
+    await ctx.send(f'bye {ctx.message.author} ')
+
+@bot.command()
 async def hello(ctx):
     await ctx.send(f'Привет! Я бот {bot.user}!')
+
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member.name} Зашёл на сервер {discord.utils.format_dt(member.joined_at)}')
